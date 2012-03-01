289798a2400b3a2a343e6510aae8cfa0855752f7
Alec Bennett <alec@alecbennett.com>
2012-03-01 13:59:07 -0900

    Add README

--- GIPL2C.c
+++ GIPL2C.c
@@ -1,8 +1,12 @@
 //---------------------------------------------------------------------------
-#include <stdio.h>
-#include <float.h>
-#include <math.h>
-#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
 #define TRUE 1
 #define FALSE 0
 
@@ -42,10 +46,6 @@ int m;
 	double L0,L1;
 
 //for memory
-extern void itwo_alloc( int ***ptr, int x, int y);
-extern void dtwo_alloc( double ***ptr, int x, int y);
-extern void d_alloc(double **var,int size);
-extern void i_alloc(int **var,int size);
 
 //for subroutines
   extern int numLayers(double depth);
@@ -54,7 +54,7 @@ extern void i_alloc(int **var,int size);
 	extern double heatCapacity (double depth0,double temper);
 	extern double unfrWater(double temp, double ac, double bc, double cc);
 	
-int main(int argc, char argv[])
+int main(int argc, char* argv[])
 {
 FILE *a_fptr;
 FILE *b_fptr;
@@ -65,7 +65,8 @@ FILE *f_fptr;
 FILE *out_fptr;
 
   int j;
-	int i, i0, k = 0;
+	int i, i0, k;
+	k = 1;
   double maxABS;
 	double E0 = 1e-6;
 	int M=numTimeSteps;
@@ -81,13 +82,13 @@ FILE *out_fptr;
 	double *U1=NULL;    
 	double *Q1=NULL;    
 	double *P1=NULL;    
-  double **T=NULL;
+  //double **T=NULL;
   double *snowDepth=NULL;
   double *snowDensity=NULL;
   double *airTemp=NULL;
   double *depth=NULL;
-  double *G0=NULL;
-  double *Timex=NULL;
+  //double *G0=NULL;
+  //double *Timex=NULL;
 
 	
 	//below extent of array default
@@ -96,75 +97,87 @@ FILE *out_fptr;
   int maxim;
   //default values below
   in_out_max = 15;
-  maxim = 200;
+  maxim = 230;
   Time_max = 1500;
 
   //allocate memory
-d_alloc(&T_init,in_out_max);
-d_alloc(&D_init,in_out_max);
-d_alloc(&D_out,in_out_max);
-d_alloc(&U1,maxim);
-d_alloc(&Q1,maxim);
-d_alloc(&P1,maxim);
-d_alloc(&snowDepth,Time_max);
-d_alloc(&snowDensity,Time_max);
-d_alloc(&airTemp,Time_max);
-d_alloc(&depth,maxim);
-dtwo_alloc(&T,Time_max,Time_max);
+T_init = new double[in_out_max];
+D_init = new double[in_out_max];
+D_out = new double[in_out_max];
+U1 = new double[maxim];
+Q1 = new double[maxim];
+P1 = new double[maxim];
+snowDepth = new double[Time_max];
+snowDensity = new double[Time_max];
+airTemp = new double[Time_max];
+depth = new double[maxim];
 
-d_alloc(&G0,Time_max);
-d_alloc(&Timex,Time_max);
- 
 
-  //open files and read files
-  //	AirTempFileName="AirTemperature.dat";
-  //	SnowCoverDepthFileName = "SnowCoverDepth.dat";
-  //	SnowCoverDensityFileName = "SnowCoverDensity.dat";
-  //	SoilDataFileNameName = "soilData.dat"; // we put here default values np
-  //	InitialTempFileName = "Initial.dat";
-  //	Depth4OutputFileName = "Depth4Output.dat";  //forget
+double **T;
+T = new double*[Time_max];
+for (int i = 0; i < Time_max; i++){
+	T[i] = new double[Time_max];
+}
 
-  //----------------  1 -------------------
-	a_fptr=fopen( "snowdepth.txt", "r");
 
-	for(i=1;i<=numTimeSteps;i++) {   //the data seems to have upto 1500 data np
-			double junk;
-			fscanf(a_fptr, "%i %lf \n", &dayNo, &junk); /* read from file */
-			snowDepth[i] = junk;
-						 }
-		fclose(a_fptr);
+double G0[Time_max];
+double Timex[Time_max];
+std::string line;
+	std::ifstream myfile;
+	myfile.open ("snowdepth.txt");
+	if (myfile.is_open()){
+		for (i = 0; i < numTimeSteps; i++){
+			getline	(myfile, line);
+			std::istringstream stm;
+			stm.str(line);
+			double tmpdayNo;
+			stm >> tmpdayNo;
+			stm >> snowDepth[i];
+		}	
+	}
+	myfile.close();
  // --------------  2  ----------------------
-	b_fptr=fopen( "Snowdensity.txt", "r");
+	myfile.open ("Snowdensity.txt");
+	if (myfile.is_open()){
+		for (i = 0; i < numTimeSteps; i++){
+			getline	(myfile, line);
+			std::istringstream stm;
+			stm.str(line);
+			double tmpdayNo;
+			stm >> tmpdayNo;
+			stm >> snowDensity[i];
+		}	
+	}
+	myfile.close();
 
-	for(i=1;i<=numTimeSteps;i++) {
-			double junk;
-			fscanf(b_fptr, "%i %lf \n", &dayNo, &junk); /* read from file */
-			snowDensity[i] = junk;			
-					 }
-		fclose(b_fptr);
 //-----------------  3 ----------
-
-    c_fptr=fopen( "AirTemperature.txt", "r");
-
-		i=0;
-	for(i=1;i<=numTimeSteps;i++) {
-			double junk;
-			fscanf(c_fptr, "%i %lf \n", &dayNo, &junk); /* read from file */
-			airTemp[i] = junk;
-							 }
-		fclose(c_fptr);
+	myfile.open ("AirTemperature.txt");
+	if (myfile.is_open()){
+		for (i = 0; i < numTimeSteps; i++){
+			getline	(myfile, line);
+			std::istringstream stm;
+			stm.str(line);
+			double tmpdayNo;
+			stm >> tmpdayNo;
+			stm >> airTemp[i];
+		}	
+	}
+	myfile.close();
 
 //------------------  4  -----------------
 
-	d_fptr=fopen( "depthGrid.txt", "r");
 
-		i=0;
-	for(i=1;i<=N;i++) {
-			double junk;
-			fscanf(d_fptr, "%lf \n", &junk); /* read from file */
-			depth[i] = junk;
-						 }
-		fclose(d_fptr);
+	myfile.open("depthGrid.txt");
+	if (myfile.is_open()){
+		for (i = 0; i < N; i++){
+			getline	(myfile, line);
+			std::istringstream stm;
+			stm.str(line);
+			stm >> depth[i];
+		}	
+	}
+	myfile.close();
+
 
  //-----------------  5  --------------------------
 
@@ -193,17 +206,23 @@ d_alloc(&Timex,Time_max);
 						 }
 		fclose(f_fptr);
 
-//--------------------------------------------------
 
+//--------------------------------------------------
 
 
     for (j=1;j<=numTimeSteps;j++){G0[j]=0.015;}
 
 //---------------------------------------------
   	//!  interpolation of the initial conditions
-	for (i = 0; i< Dn; i++){//do i=1,Dn
-		T[i][0]=T_init[k-1]+ ((T_init[k]-T_init[k-1]) /
-		(D_init[k]-D_init[k-1])  *  (depth[i]-D_init[k-1])) ;
+	for (i = 0; i < Dn; i++){//do i=1,Dn
+		double dividend = T_init[k]-T_init[k-1];
+		double divisor = (D_init[k]-D_init[k-1]) * (depth[i]-D_init[k-1]);
+		//T[i][0]=T_init[k-1]+ ((T_init[k]-T_init[k-1]) / (D_init[k]-D_init[k-1])  *  (depth[i]-D_init[k-1])) ;
+		if (divisor != 0){
+			T[i][0] = T_init[k-1] + dividend / divisor;
+		} else {
+			T[i][0] = 0;
+		}
 		  U1[i]=T[i][0];
 		if (depth[i]==D_init[k]) k=k+1;
 		printf ("i = %i k = %i Init T %f Init D %f\n", i,k,T_init[i], D_init[i]);
@@ -236,10 +255,10 @@ d_alloc(&Timex,Time_max);
 				L1=soilThermalConductivity(depth[1],U1[1]);   ///main solution para/lamda 
 
 				H0=depth[1]-depth[0];
-				if (snowDepth[j] < E0 || T[1][j-1] > E0){
+				if (snowDepth[j] < E0 || T[0][j-1] > E0){
 					G1=0.0;
 					G2=airTemp[j];
-				}else if (T[1][j-1] <= 0.0 &&  snowDepth[j] < E0) {
+				}else if (T[0][j-1] <= 0.0 &&  snowDepth[j] < E0) {
 					     G1=0.0;
 						 G2=airTemp[j];
 				}
@@ -396,106 +415,4 @@ double snowProperties (int j0, double *snowDensity, double *snowDepth)
 
 return unWater;
 	}
-
-
-
-
-
-  void itwo_alloc(int ***array,int rows, int cols)
-{
-int  i,frows,fcols, numgood=0;
-int error=0;
-
-if ((rows==0)||(cols==0))
-  {
-  printf("Error: Attempting to allocate array of size 0\n");
-  exit;
-  }
-
-frows=rows+1;  /* added one for FORTRAN numbering */
-fcols=cols+1;  /* added one for FORTRAN numbering */
-
-*array=(int **)malloc(frows*sizeof(int *));
-if (*array) 
-  {
-  memset((*array), 0, frows*sizeof(int*));
-  for (i=0; i<frows; i++)
-    {
-    (*array)[i] =(int *)malloc(fcols*sizeof(int ));
-    if ((*array)[i] == NULL)
-      {
-      error = 1;
-      numgood = i;
-      i = frows;
-      }
-     else memset((*array)[i], 0, fcols*sizeof(int )); 
-     }
-   }
-return;
-}
-
-
-
-void dtwo_alloc(double ***array,int rows, int cols)
-{
-int  i,frows,fcols, numgood=0;
-int error=0;
-
-if ((rows==0)||(cols==0))
-  {
-  printf("Error: Attempting to allocate array of size 0\n");
-  exit;
-  }
-
-frows=rows+1;  /* added one for FORTRAN numbering */
-fcols=cols+1;  /* added one for FORTRAN numbering */
-
-*array=(double **)malloc(frows*sizeof(double *));
-if (*array) 
-  {
-  memset((*array), 0, frows*sizeof(double *));
-  for (i=0; i<frows; i++)
-    {
-    (*array)[i] =(double *)malloc(fcols*sizeof(double ));
-    if ((*array)[i] == NULL)
-      {
-      error = 1;
-      numgood = i;
-      i = frows;
-      }
-     else memset((*array)[i], 0, fcols*sizeof(double )); 
-     }
-   }
-return;
-}
-
-
-
-void d_alloc(double **var,int size)
-{
-  size++;  /* just for safety */
-
-   *var = (double *)malloc(size * sizeof(double));
-   if (*var == NULL)
-      {
-      printf("Problem allocating memory for array in d_alloc.\n");
-      return;
-      }
-   else memset(*var,0,size*sizeof(double));
-   return;
-}
-
-void i_alloc(int **var,int size)
-{
-   size++;  /* just for safety */
-
-   *var = (int *)malloc(size * sizeof(int));
-   if (*var == NULL)
-      {
-      printf("Problem allocating memory in i_alloc\n");
-      return; 
-      }
-   else memset(*var,0,size*sizeof(int));
-   return;
-}
 
